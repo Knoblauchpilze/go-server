cead3d8829ebb520c3fd694785fd794ecf15370b
KnoblauchPilze <prolific-programmer@tuta.com>
2023-03-15 08:32:18 +0100

    feat: Merged server_response and response

--- pkg/rest/response_test.go
+++ pkg/rest/response_test.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"testing"
@@ -10,6 +11,8 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
+
+var dummyID = "eb10f542-c2a8-11ed-befe-18c04d0e6a41"
 
 func generateResponseWithBody(body interface{}) *http.Response {
 	resp := http.Response{
@@ -27,6 +30,127 @@ func generateResponseWithBody(body interface{}) *http.Response {
 	resp.Body = io.NopCloser(rdr)
 
 	return &resp
+}
+
+func TestResponse_OK(t *testing.T) {
+	assert := assert.New(t)
+
+	id, _ := uuid.Parse(dummyID)
+	mrw := mockResponseWriter{}
+
+	resp := NewSuccessResponse(id)
+	resp.Write(&mrw)
+
+	var expected = fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\"}", dummyID, StatusOK)
+	assert.Equal(expected, string(mrw.data))
+}
+
+func TestResponse_NOK(t *testing.T) {
+	assert := assert.New(t)
+
+	id, _ := uuid.Parse(dummyID)
+	mrw := mockResponseWriter{}
+
+	resp := NewErrorResponse(id)
+	resp.Write(&mrw)
+
+	var expected = fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\"}", dummyID, StatusNOK)
+	assert.Equal(expected, string(mrw.data))
+}
+
+func TestResponse_Pass(t *testing.T) {
+	assert := assert.New(t)
+
+	id, _ := uuid.Parse(dummyID)
+	mrw := mockResponseWriter{}
+
+	resp := NewErrorResponse(id)
+	resp.Pass()
+	resp.Write(&mrw)
+
+	expected := fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\"}", dummyID, StatusOK)
+	assert.Equal(expected, string(mrw.data))
+}
+
+func TestResponse_Fail(t *testing.T) {
+	assert := assert.New(t)
+
+	id, _ := uuid.Parse(dummyID)
+	mrw := mockResponseWriter{}
+
+	resp := NewSuccessResponse(id)
+	resp.Fail()
+	resp.Write(&mrw)
+
+	expected := fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\"}", dummyID, StatusNOK)
+	assert.Equal(expected, string(mrw.data))
+}
+
+func TestResponse_WithDetails(t *testing.T) {
+	assert := assert.New(t)
+
+	id, _ := uuid.Parse(dummyID)
+	mrw := mockResponseWriter{}
+
+	resp := NewSuccessResponse(id)
+	resp.WithDetails(23)
+	resp.Write(&mrw)
+
+	expected := fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\",\"Details\":23}", dummyID, StatusOK)
+	assert.Equal(expected, string(mrw.data))
+
+	test := foo{
+		Bar: "haha",
+		Baz: -23,
+	}
+	resp.WithDetails(test)
+	resp.Write(&mrw)
+
+	expected = fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\",\"Details\":{\"Bar\":\"haha\",\"Baz\":-23}}", dummyID, StatusOK)
+	assert.Equal(expected, string(mrw.data))
+}
+
+func TestResponse_WithCode(t *testing.T) {
+	assert := assert.New(t)
+
+	id, _ := uuid.Parse(dummyID)
+	mrw := mockResponseWriter{}
+
+	resp := NewSuccessResponse(id)
+	resp.WithCode(http.StatusOK)
+	resp.Write(&mrw)
+
+	expected := fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\"}", dummyID, StatusOK)
+	assert.Equal(expected, string(mrw.data))
+	assert.Equal(http.StatusOK, mrw.code)
+
+	resp.WithCode(http.StatusTeapot)
+	resp.Write(&mrw)
+
+	expected = fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\"}", dummyID, StatusNOK)
+	assert.Equal(expected, string(mrw.data))
+	assert.Equal(http.StatusTeapot, mrw.code)
+}
+
+func TestResponse_Write(t *testing.T) {
+	assert := assert.New(t)
+
+	id, _ := uuid.Parse(dummyID)
+	mrw := mockResponseWriter{}
+
+	resp := NewSuccessResponse(id)
+	resp.Write(&mrw)
+
+	expected := fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\"}", dummyID, StatusOK)
+	assert.Equal(expected, string(mrw.data))
+	assert.Equal(http.StatusOK, mrw.code)
+
+	resp.WithDetails(12.2)
+	resp.Write(&mrw)
+
+	expected = fmt.Sprintf("{\"RequestID\":\"%s\",\"Status\":\"%v\",\"Details\":12.2}", dummyID, StatusOK)
+	assert.Equal(expected, string(mrw.data))
+	assert.Equal(http.StatusOK, mrw.code)
 }
 
 func TestGetBodyFromHttpResponseAs_InvalidResponse(t *testing.T) {
