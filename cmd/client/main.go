c04ce182abe197980cf5682f63c458b8732cdf62
KnoblauchPilze <prolific-programmer@tuta.com>
2023-03-15 08:36:34 +0100

    feat: Ground work for commands in client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -9,17 +9,33 @@ import (
 )
 
 func main() {
+	if len(os.Args) == 1 {
+		logrus.Errorf("No command specified, aborting")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "signup":
+		signUp()
+	default:
+		logrus.Errorf("Unknown command \"%s\"", command)
+	}
+}
+
+func signUp() {
 	userData := types.UserData{
 		Name:     "toto",
 		Password: "123456",
 	}
 
-	if len(os.Args) > 1 {
-		userData.Name = os.Args[1]
+	if len(os.Args) > 2 {
+		userData.Name = os.Args[2]
 	}
 
-	if len(os.Args) > 2 {
-		userData.Password = os.Args[2]
+	if len(os.Args) > 3 {
+		userData.Password = os.Args[3]
 	}
 
 	signUp, err := connection.SignUp(userData)
