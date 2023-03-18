17c8ce7e4e00d297d589de918f448413ba7d3e7b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-03-18 09:29:48 +0100

    feat: Ground work for token authentication

--- internal/routes/authentication_context.go
+++ internal/routes/authentication_context.go
@@ -4,50 +4,97 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/KnoblauchPilze/go-server/pkg/auth"
 	"github.com/KnoblauchPilze/go-server/pkg/errors"
 	"github.com/KnoblauchPilze/go-server/pkg/rest"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
 var authenticationScheme = "bearer"
+var authenticationUserKey = "user"
+var authenticationTokenKey = "token"
 
-func authenticationCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// https://stackoverflow.com/questions/33265812/best-http-authorization-header-type-for-jwt
-		// https://reqbin.com/req/5k564bhv/get-request-bearer-token-authorization-header-example
-		// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization
-		reqData, ok := getRequestDataFromContextOrFail(w, r)
-		if !ok {
-			return
-		}
+func generateAuthenticationContext(tokens auth.Auth) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// https://stackoverflow.com/questions/33265812/best-http-authorization-header-type-for-jwt
+			// https://reqbin.com/req/5k564bhv/get-request-bearer-token-authorization-header-example
+			// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization
+			reqData, ok := getRequestDataFromContextOrFail(w, r)
+			if !ok {
+				return
+			}
 
-		authData, err := rest.GetSingleHeaderFromHttpRequest(r, "Authorization")
-		if err != nil {
-			reqData.failWithErrorAndCode(err, http.StatusBadRequest, w)
-			return
-		}
+			authData, err := rest.GetSingleHeaderFromHttpRequest(r, "Authorization")
+			if err != nil {
+				reqData.failWithErrorAndCode(err, http.StatusBadRequest, w)
+				return
+			}
 
-		token, err := checkAndConsumeScheme(authData)
-		if err != nil {
-			reqData.failWithErrorAndCode(err, http.StatusBadRequest, w)
-			return
-		}
+			token, err := parseAuthenticationHeader(authData)
+			if err != nil {
+				reqData.failWithErrorAndCode(err, http.StatusBadRequest, w)
+				return
+			}
 
-		logrus.Warnf("Should check token: %v", token)
+			check, err := tokens.GetToken(token.User)
 
-		next.ServeHTTP(w, r)
-	})
+			logrus.Warnf("Should check token: %+v (%+v, %+v)", token, check, err)
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
-func checkAndConsumeScheme(authData string) (string, error) {
+func parseAuthenticationHeader(authData string) (auth.Token, error) {
+	var out auth.Token
+	var genErrMsg = "invalid authentication header"
+
 	tokens := strings.Split(authData, " ")
-	if len(tokens) != 2 {
-		return "", errors.New("invalid authentication header")
+	if len(tokens) != 3 {
+		return out, errors.New(genErrMsg)
 	}
 
 	if strings.ToLower(tokens[0]) != authenticationScheme {
-		return "", errors.New("invalid authentication scheme")
+		return out, errors.New(genErrMsg)
 	}
 
-	return tokens[1], nil
+	props := make(map[string]string)
+	tokens = tokens[1:]
+	for _, prop := range tokens {
+		keyValue := strings.Split(prop, "=")
+		if len(keyValue) != 2 {
+			err := errors.Newf("Ill-formed prop in Authorization header: \"%s\"", prop)
+			return out, errors.Wrap(err, genErrMsg)
+		}
+		if len(keyValue[0]) == 0 || len(keyValue[1]) == 0 {
+			err := errors.Newf("Ill-formed prop in Authorization header: \"%s\"", prop)
+			return out, errors.Wrap(err, genErrMsg)
+		}
+
+		props[keyValue[0]] = keyValue[1]
+	}
+
+	userID, ok := props[authenticationUserKey]
+	if !ok {
+		err := errors.Newf("no \"%s\" key in Authorization header", authenticationUserKey)
+		return out, errors.Wrap(err, genErrMsg)
+	}
+	id, err := uuid.Parse(userID)
+	if err != nil {
+		err := errors.Wrapf(err, "failed to parse user id \"%s\"", userID)
+		return out, errors.Wrap(err, genErrMsg)
+	}
+
+	token, ok := props[authenticationTokenKey]
+	if !ok {
+		err := errors.Newf("no \"%s\" key in Authorization header", authenticationTokenKey)
+		return out, errors.Wrap(err, genErrMsg)
+	}
+
+	out.User = id
+	out.Value = token
+
+	return out, nil
 }
