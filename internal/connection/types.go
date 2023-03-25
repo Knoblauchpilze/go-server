89418a7bc3aefa58194680113361cb8233fd7662
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-03-25 16:40:44 +0100

    feat: Token is provided through arguments, no automatic signup/login

--- internal/connection/types.go
+++ internal/connection/types.go
@@ -1,6 +1,7 @@
 package connection
 
 import (
+	"github.com/KnoblauchPilze/go-server/pkg/auth"
 	"github.com/KnoblauchPilze/go-server/pkg/types"
 	"github.com/google/uuid"
 )
@@ -10,6 +11,8 @@ var serverURL = "http://localhost:3000"
 type Session interface {
 	SignUp(in types.UserData) error
 	Login(in types.UserData) error
+
+	Authenticate(token auth.Token) error
 
 	ListUsers() ([]uuid.UUID, error)
 }
