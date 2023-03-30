787374d05bec7243cbe1c9fc46448056df766e27
KnoblauchPilze <prolific-programmer@tuta.com>
2023-03-30 22:12:39 +0200

    feat: List users first version

--- cmd/client/cmd/list/users.go
+++ cmd/client/cmd/list/users.go
@@ -1,7 +1,11 @@
 package list
 
 import (
+	"time"
+
 	"github.com/KnoblauchPilze/go-server/internal/connection"
+	"github.com/KnoblauchPilze/go-server/pkg/auth"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -9,18 +13,24 @@ import (
 var usersCmd = &cobra.Command{
 	Use:   "users",
 	Short: "List all registered users",
+	Args:  cobra.ExactArgs(2),
 	Run:   usersCmdBody,
 }
 
 func usersCmdBody(cmd *cobra.Command, args []string) {
-	sess := connection.NewSession()
-
-	token, err := fetchUserToken()
+	id, err := uuid.Parse(args[0])
 	if err != nil {
-		logrus.Fatalf("Failed to list users: %+v", err)
+		logrus.Errorf("invalid user id provided (%v)", err)
 		return
 	}
 
+	token := auth.Token{
+		User:       id,
+		Value:      args[1],
+		Expiration: time.Now().Add(1 * time.Minute),
+	}
+
+	sess := connection.NewSession()
 	if err := sess.Authenticate(token); err != nil {
 		logrus.Fatalf("Failed to list users: %+v", err)
 		return
