f2b5519a4217caedbf3626cecc3e55d7fab81ed2
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-03-31 18:16:19 +0200

    feat: Refactored connection package to extract get/post requests

--- internal/connection/session.go
+++ internal/connection/session.go
@@ -2,10 +2,15 @@ package connection
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/KnoblauchPilze/go-server/pkg/auth"
+	"github.com/KnoblauchPilze/go-server/pkg/connection"
 	"github.com/KnoblauchPilze/go-server/pkg/errors"
+	"github.com/KnoblauchPilze/go-server/pkg/rest"
+	"github.com/KnoblauchPilze/go-server/pkg/types"
+	"github.com/KnoblauchPilze/go-server/pkg/users"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
@@ -17,6 +22,48 @@ type sessionImpl struct {
 
 func NewSession() Session {
 	return &sessionImpl{}
+}
+
+func (si *sessionImpl) SignUp(in types.UserData) error {
+	var out types.SignUpResponse
+
+	url := fmt.Sprintf("%s/signup", serverURL)
+	req := connection.NewPostRequest(url, http.Header{}, "application/json", in)
+	resp, err := req.Perform()
+	if err != nil {
+		return err
+	}
+
+	err = rest.GetBodyFromHttpResponseAs(resp, &out)
+	if err != nil {
+		return err
+	}
+
+	si.userId = out.Id
+	logrus.Infof("Signed up with id %v", si.userId)
+
+	return nil
+}
+
+func (si *sessionImpl) Login(in types.UserData) error {
+	var out types.LoginResponse
+
+	url := fmt.Sprintf("%s/login", serverURL)
+	req := connection.NewPostRequest(url, http.Header{}, "application/json", in)
+	resp, err := req.Perform()
+	if err != nil {
+		return err
+	}
+
+	err = rest.GetBodyFromHttpResponseAs(resp, &out)
+	if err != nil {
+		return err
+	}
+
+	si.token = out.Token
+	logrus.Infof("Logged in, active token is %+v", si.token)
+
+	return nil
 }
 
 func (si *sessionImpl) Authenticate(token auth.Token) error {
@@ -31,6 +78,62 @@ func (si *sessionImpl) Authenticate(token auth.Token) error {
 	si.token = token
 
 	return nil
+}
+
+func (si *sessionImpl) ListUsers() ([]uuid.UUID, error) {
+	var out []uuid.UUID
+
+	listUsersURL := fmt.Sprintf("%s/users", serverURL)
+
+	auth, err := si.generateAuthenticationHeader()
+	if err != nil {
+		return out, err
+	}
+
+	headers := map[string][]string{
+		"Authorization": {auth},
+	}
+
+	req := connection.NewGetRequest(listUsersURL, headers)
+	resp, err := req.Perform()
+	if err != nil {
+		return out, err
+	}
+
+	err = rest.GetBodyFromHttpResponseAs(resp, &out)
+	if err != nil {
+		return out, errors.WrapCode(err, errors.ErrGetRequestFailed)
+	}
+
+	return out, nil
+}
+
+func (si *sessionImpl) ListUser(id uuid.UUID) (users.User, error) {
+	var out users.User
+
+	listUserURL := fmt.Sprintf("%s/users/%s", serverURL, id)
+
+	auth, err := si.generateAuthenticationHeader()
+	if err != nil {
+		return out, err
+	}
+
+	headers := map[string][]string{
+		"Authorization": {auth},
+	}
+
+	req := connection.NewGetRequest(listUserURL, headers)
+	resp, err := req.Perform()
+	if err != nil {
+		return out, err
+	}
+
+	err = rest.GetBodyFromHttpResponseAs(resp, &out)
+	if err != nil {
+		return out, errors.WrapCode(err, errors.ErrGetRequestFailed)
+	}
+
+	return out, nil
 }
 
 func (si *sessionImpl) generateAuthenticationHeader() (string, error) {
---
+++ pkg/connection/request.go
@@ -0,0 +1,55 @@
+package connection
+
+import (
+	"net/http"
+
+	"github.com/KnoblauchPilze/go-server/pkg/errors"
+)
+
+type requestBuilder func(ri *requestImpl) (*http.Request, error)
+
+type requestImpl struct {
+	url     string
+	headers http.Header
+	builder requestBuilder
+}
+
+func newRequest(url string, headers http.Header, builder requestBuilder) Request {
+	return &requestImpl{
+		url:     url,
+		headers: headers,
+		builder: builder,
+	}
+}
+
+func (ri *requestImpl) WithUrl(url string) Request {
+	ri.url = url
+	return ri
+}
+
+func (ri *requestImpl) WithHeaders(headers http.Header) Request {
+	ri.headers = headers
+	return ri
+}
+
+func (ri *requestImpl) Perform() (*http.Response, error) {
+	var resp *http.Response
+	var err error
+
+	req, err := ri.builder(ri)
+	if req == nil {
+		return resp, errors.New("invalid request to perform")
+	}
+	if err != nil {
+		return resp, errors.Wrap(err, "failed to build http request")
+	}
+
+	client := http.Client{}
+
+	resp, err = client.Do(req)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
